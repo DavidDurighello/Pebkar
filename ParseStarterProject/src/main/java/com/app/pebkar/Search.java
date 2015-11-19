76f988114e271e4161d302c538be6e7b232d26f5
Lyyn <lyynou@gmail.com>
2015-11-19 16:26:06 +0100

    Tu ne devineras jamais ce que contient ce commit...

--- ParseStarterProject/src/main/java/com/app/pebkar/Search.java
+++ ParseStarterProject/src/main/java/com/app/pebkar/Search.java
@@ -2,11 +2,16 @@ package com.app.pebkar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Search extends AppCompatActivity {
@@ -19,19 +24,25 @@ public class Search extends AppCompatActivity {
 
         ListeCovoiturageDB lc = new ListeCovoiturageDB();
         ParseQuery<ListeCovoiturageDB> query = ParseQuery.getQuery(ListeCovoiturageDB.class);
-        query.whereNotEqualTo("idListeCovoiturage",0);
+        query.whereNotEqualTo("idListeCovoiturage", 0);
         query.findInBackground(new FindCallback<ListeCovoiturageDB>() {
             @Override
             public void done(List<ListeCovoiturageDB> listeCovoiturage, ParseException e) {
-                if(e==null){
+                if (e == null) {
                     int i = 0;
-                    for(ListeCovoiturageDB lc : listeCovoiturage){
-                        System.out.println(lc.get("idListeCovoiturage")+": "+lc.get("lieudepart")+" "+lc.get("lieuarrivee")+" "+lc.get("datedepart")+" "+lc.get("datearrivee"));
+                    for (ListeCovoiturageDB lc : listeCovoiturage) {
+                        System.out.println(lc.get("idListeCovoiturage") + ": " + lc.get("lieudepart") + " " + lc.get("lieuarrivee") + " " + lc.get("datedepart") + " " + lc.get("datearrivee"));
                     }
                 } else {
                     System.out.println(e.getMessage());
                 }
             }
         });
+
+        ListView lv_search = (ListView) findViewById(R.id.lv_search);
+        List<String> li_search = new ArrayList<String>();
+        li_search.add("test");
+        ArrayAdapter<String> aa_search = new ArrayAdapter<String>(getApplicationContext(), R.layout.activity_search, li_search);
+        lv_search.setAdapter(aa_search);
     }
 }
