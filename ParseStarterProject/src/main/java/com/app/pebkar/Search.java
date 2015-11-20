c7afcdcf3b7a938073837f08ff9ee58444fe741d
Lyyn <lyynou@gmail.com>
2015-11-20 22:38:51 +0100

    Search.java     La recherche marche mais y'a toujours le problème d'affichage. C'est du au fait que ça se fait en background (sur un thread en background). Faut trouver le moyen, via un listener ou une autre méthode de lancer le rafraîchissement de la ListView. Je fais ce commit pour garder une sauvegarde propre d'un truc qui marche. Btw après plusieurs heures je pense avoir trouvé le moyen de faire ça. À tester donc.

--- ParseStarterProject/src/main/java/com/app/pebkar/Search.java
+++ ParseStarterProject/src/main/java/com/app/pebkar/Search.java
@@ -6,36 +6,62 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.app.pebkar.Modele.ListeCovoiturageDB;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Search extends AppCompatActivity {
+
+    private ListView listViewSearch;
+    private List<String> listSearch;
+    private ListeCovoiturageDB listeCovoiturageDB;
+    private ArrayAdapter<String> arrayAdapterSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        ListeCovoiturageDB listeCovoiturageDB = new ListeCovoiturageDB();
-        ListView listViewSearch = (ListView) findViewById(R.id.lv_search);
-        List<String> listSearch = null;
-        try {
-            listSearch = listeCovoiturageDB.readData();
-            for (String lc:listSearch) {
-                System.out.println(lc);
+        listSearch = new ArrayList<String>();
+        listViewSearch = (ListView) findViewById(R.id.lv_search);
+        arrayAdapterSearch = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listSearch);
+        listViewSearch.setAdapter(arrayAdapterSearch);
 
+        chargerVoyages();
+    }
+
+    /**
+     * Récupère tous les voyages et les stocke dans listSearch
+     * Méthode utilisée également pour la recherche
+     */
+    protected void chargerVoyages() {
+        listeCovoiturageDB = new ListeCovoiturageDB();
+        String rech_ville = ((EditText) findViewById(R.id.et_ville)).getText().toString(); // Ville recherchée
+        System.out.println("[DEBUG] rech_ville : " + rech_ville);
+
+        try {
+
+            if(rech_ville.isEmpty()) {
+                listSearch = listeCovoiturageDB.readData();
             }
-            ArrayAdapter<String> arrayAdapterSearch;
-            arrayAdapterSearch = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, listSearch);
-            listViewSearch.setAdapter(arrayAdapterSearch);
+            else {
+                listSearch = listeCovoiturageDB.readData(rech_ville);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        arrayAdapterSearch = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listSearch);
+        listViewSearch.setAdapter(arrayAdapterSearch);
+        listViewSearch.invalidateViews();
     }
 
+    /*
     public void btn_filtre(View view){
         EditText editTextSearch = (EditText) findViewById(R.id.et_ville);
         ListeCovoiturageDB listeCovoiturageDB = new ListeCovoiturageDB();
@@ -53,5 +79,10 @@ public class Search extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    */
+
+    public void btn_filtre(View view) {
+        chargerVoyages();
     }
 }
