b6d7dd4a8bdbfe24659148ef8cd9dfa41b791ee6
elykx <elykx@live.fr>
2015-11-30 20:33:04 +0100

    Test Facebook

--- ParseStarterProject/src/main/java/com/app/pebkar/Modele/Crud.java
+++ ParseStarterProject/src/main/java/com/app/pebkar/Modele/Crud.java
@@ -6,8 +6,8 @@ import java.util.List;
  * Created by David Elykx on 19-11-15.
  */
 public interface Crud {
-    public void createData() throws Exception;
-    public List readData() throws Exception;
-    public void updateData() throws Exception;
-    public void deleteData() throws Exception;
+    void createData() throws Exception;
+    List readData() throws Exception;
+    void updateData() throws Exception;
+    void deleteData() throws Exception;
 }
