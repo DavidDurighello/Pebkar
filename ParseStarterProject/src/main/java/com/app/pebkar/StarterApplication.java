629e82e07e6f24b22b5a6ff4625702db6b416376
elykx <elykx@live.fr>
2015-11-30 20:34:34 +0100

    Revert "Revert "Test Facebook""
    This reverts commit cffcec93f11c63ecf2138e6d9bf9d616b95a06f3.

--- ParseStarterProject/src/main/java/com/app/pebkar/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/app/pebkar/StarterApplication.java
@@ -11,41 +11,49 @@ package com.app.pebkar;
 import android.app.Application;
 
 import com.app.pebkar.Modele.ListeCovoiturage;
-import com.app.pebkar.Modele.ListeCovoiturageDB;
 import com.app.pebkar.Modele.Notation;
 import com.app.pebkar.Modele.Passagers;
 import com.app.pebkar.Modele.Profil;
 import com.app.pebkar.Modele.TableSeq;
+import com.facebook.FacebookSdk;
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 
 public class StarterApplication extends Application {
 
+  static final String TAG = "Pebkar";
+
   @Override
   public void onCreate() {
     super.onCreate();
+
+    FacebookSdk.sdkInitialize(getApplicationContext());
 
     // Enable Local Datastore.
     Parse.enableLocalDatastore(this);
 
     //DBClass
     ParseObject.registerSubclass(TableSeq.class);
-    //ParseObject.registerSubclass(ListeCovoiturageDB.class);
     ParseObject.registerSubclass(ListeCovoiturage.class);
     ParseObject.registerSubclass(Notation.class);
     ParseObject.registerSubclass(Passagers.class);
     ParseObject.registerSubclass(Profil.class);
 
     // Add your initialization code here
-    Parse.initialize(this, "fc7IlnwGrW6KgP43o6dHNir9W20xzqEhr94EMVyW","BMclWPRzlY4yn5yR7ZdbyOXkohQVJOIoBnBwfi2I");
+    Parse.initialize(this, "yfghOtaaLhByvSHxDnEODKwlixla8XnJcMtICtxV", "htrSP1mnDwBmv5ggvIxvmRSD8XZ8bnfi8YSwIf9e");
+    ParseFacebookUtils.initialize(this);
+
 
     ParseUser.enableAutomaticUser();
     ParseACL defaultACL = new ParseACL();
     // Optionally enable public read access.
-    // defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicReadAccess(true);
     ParseACL.setDefaultACL(defaultACL, true);
   }
+
+
 }
